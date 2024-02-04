f0f0a7eecae136c20b62e000903e659710d47179
Hiroaki Suzuki <rirazou05410@gmail.com>
2024-02-04 14:15:02 +0900

    ローカルでの送受信の処理

---
+++ app/src/receive.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config'
+import { DeleteMessageCommand, ReceiveMessageCommand, SQSClient } from '@aws-sdk/client-sqs'
+import { Message } from '@aws-sdk/client-sqs/dist-types/models/models_0'
+import { queueUrl } from './utils'
+
+const client = new SQSClient({ region: 'ap-northeast-1' })
+
+async function receive() {
+  const params = {
+    QueueUrl: queueUrl
+  }
+  const command = new ReceiveMessageCommand(params)
+  const response = await client.send(command)
+
+  if (response.Messages) {
+    for (const message of response.Messages) {
+      console.log(`次のメッセージは消費しました。 ${message.Body}`)
+      await deleteMessage(message)
+    }
+  } else {
+    console.log('メッセージはありません')
+  }
+}
+
+async function deleteMessage(message: Message): Promise<void> {
+  const params = {
+    QueueUrl: queueUrl,
+    ReceiptHandle: message.ReceiptHandle
+  }
+  const command = new DeleteMessageCommand(params)
+  const response = await client.send(command)
+  console.log(response)
+  console.log(`次のメッセージは削除しました。 ${message.Body}`)
+}
+
+receive()
+  .then(() => {
+    console.log('Done')
+  })
+  .catch(error => {
+    console.error(error)
+  })
---
+++ app/src/utils.ts
@@ -0,0 +1,1 @@
+export const queueUrl = process.env.QUEUE_URL
